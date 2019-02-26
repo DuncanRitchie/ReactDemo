40be1eb80bf689bf148bbc7efb31481bcf8a823e
Duncan Ritchie <codenation@Codes-MBP.lan>
2019-02-26 14:08:21 +0000

    Updates

---
+++ precache-manifest.f0927347c20bbc3be14510300f3b66e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1140bd63ef71a0568e91",
+    "url": "/ReactDemo/static/js/runtime~main.1140bd63.js"
+  },
+  {
+    "revision": "154666edf35485bd3f8a",
+    "url": "/ReactDemo/static/js/main.154666ed.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/ReactDemo/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "154666edf35485bd3f8a",
+    "url": "/ReactDemo/static/css/main.a248273b.chunk.css"
+  },
+  {
+    "revision": "24dc6daf8edf81c5d8824a83a5889a4a",
+    "url": "/ReactDemo/index.html"
+  }
+];
