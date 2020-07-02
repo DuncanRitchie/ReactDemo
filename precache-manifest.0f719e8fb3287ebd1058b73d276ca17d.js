316fa8ff84db52e7506e733dd25401acc1f7c388
Duncan Ritchie <duncan.ritchie@informationcatalyst.com>
2020-07-02 20:34:47 +0100

    Updates

---
+++ precache-manifest.0f719e8fb3287ebd1058b73d276ca17d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7ec35a3bbb8152837af012aea752e6a",
+    "url": "/react-demo/index.html"
+  },
+  {
+    "revision": "cacbea2a2997c4e45f10",
+    "url": "/react-demo/static/css/main.d63d76f2.chunk.css"
+  },
+  {
+    "revision": "3c290ca2fccc07a4b4a9",
+    "url": "/react-demo/static/js/2.e4126d1c.chunk.js"
+  },
+  {
+    "revision": "0261e2dee4a7e8969069abb914658242",
+    "url": "/react-demo/static/js/2.e4126d1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cacbea2a2997c4e45f10",
+    "url": "/react-demo/static/js/main.f3729272.chunk.js"
+  },
+  {
+    "revision": "118931e8f5916cd2bcd0",
+    "url": "/react-demo/static/js/runtime-main.683407a3.js"
+  }
+]);
